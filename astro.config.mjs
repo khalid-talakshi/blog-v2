5af4e286cc1bedc238b0612e2ede5878b60fb99d
Khalid Talakshi <khalid.talakshi@bluejays.com>
2026-03-20 09:22:20 -0400

    setup plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+  fonts:[{
+    provider: fontProviders.google(),
+    name: "Anta",
+    cssVariable: "--font-anta"
+  }],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
