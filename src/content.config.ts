9c87df5759a27eb8ec3b1259ebd3a9520a6bf11f
Khalid Talakshi <khalid.talakshi@outlook.com>
2026-03-21 14:15:09 -0400

    made blog post collection and page

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string()
+  }),
+});
+
+// Expose your defined collection to Astro
+// with the `collections` export
+export const collections = { blog };
