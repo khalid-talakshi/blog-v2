79252159cecc3665a3bd76b5446df822771f534c
Khalid Talakshi <khalid.talakshi@bluejays.com>
2026-03-23 08:52:41 -0400

    renamed to linegraph

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -3,13 +3,13 @@ import { RechartsDevtools } from '@recharts/devtools';
 import { CustomTooltip } from './ChartTooltip';
 import { FaMagnifyingGlassMinus } from "react-icons/fa6";
 import {  useGraphZoom } from '../hooks/useGraphZoom';
-import {type Trace} from '../types/'
+import {type Trace} from '../types'
 
 export interface Props {
   traces: Trace[]
 }
 
-export default function Chart({ traces }: Props) {
+export default function LineGraph({ traces }: Props) {
   const {left, right, zoomOut, zoom, onMouseDown, onMouseMove, refAreaLeft, refAreaRight, bottom, top} = useGraphZoom(traces)
 
   const lines = traces?.map((trace) => (
@@ -18,10 +18,10 @@ export default function Chart({ traces }: Props) {
 
   return (
     <div className='flex flex-col gap-2'>
-      <div className='flex gap-2 border border-white w-full rounded-xl text-sm px-2 py-1'>
-        <div onClick={zoomOut}><FaMagnifyingGlassMinus /></div>
+      <div className='flex gap-2 w-full rounded-xl text-sm px-2 py-1'>
+        <div onClick={zoomOut} className='hover:cursor-pointer hover:text-gray-400 transition ease-in-out duration-75'><FaMagnifyingGlassMinus /></div>
       </div>
-      <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }} responsive onMouseDown={onMouseDown} onMouseUp={zoom} onMouseMove={onMouseMove}>
+      <LineChart style={{ width: '100%', aspectRatio: 1.618 }} responsive onMouseDown={onMouseDown} onMouseUp={zoom} onMouseMove={onMouseMove}>
         <CartesianGrid strokeDasharray="2 2" stroke='gray' />
         {lines}
         <XAxis dataKey="distance" allowDecimals={false} type="number" tickCount={4} domain={[left, right]} allowDataOverflow />
